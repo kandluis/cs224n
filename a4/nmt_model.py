371f7211c61c707c0adcfbf921c873ccf6c33a28
Luis Perez <luis.perez.live@gmail.com>
2019-02-03 19:48:51 -0800

    Fixing tuple unpacking

--- a4/nmt_model.py
+++ a4/nmt_model.py
@@ -267,8 +267,8 @@ class NMT(nn.Module):
     for Y_t in torch.split(Y, split_size_or_sections=1):
       Y_t = torch.squeeze(Y_t, dim=0)
       Ybar_t = torch.cat((Y_t, o_prev), dim=1)
-      dec_state, o_t, _, = self.step(Ybar_t, dec_state, enc_hiddens,
-                                     enc_hiddens_proj, enc_masks)
+      dec_state, o_t, _ = self.step(Ybar_t, dec_state, enc_hiddens,
+                                    enc_hiddens_proj, enc_masks)
       combined_outputs.append(o_t)
       o_prev = o_t
     combined_outputs = torch.stack(combined_outputs)
